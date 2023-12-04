8f1188ce57cb38a8c4aa53198282572ac4607406
Khristoffer Julio <khristoffer.julio@rsvp.com.au>
2023-12-05 07:32:34 +0800

    Initial Commit

---
+++ RickAndMortyFeed/RickAndMortyFeed.h
@@ -0,0 +1,18 @@
+//
+//  RickAndMortyFeed.h
+//  RickAndMortyFeed
+//
+//  Created by Khristoffer Julio on 12/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RickAndMortyFeed.
+FOUNDATION_EXPORT double RickAndMortyFeedVersionNumber;
+
+//! Project version string for RickAndMortyFeed.
+FOUNDATION_EXPORT const unsigned char RickAndMortyFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RickAndMortyFeed/PublicHeader.h>
+
+
